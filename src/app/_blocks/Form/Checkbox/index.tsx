cc39a7dd3ebf1d14a5cf93516bbe36c19f0fa24d
Nick Mantia <nickmantia@Drews-MacBook.lan>
2024-08-12 15:15:36 -0500

    fix import

--- src/app/_blocks/Form/Checkbox/index.tsx
+++ src/app/_blocks/Form/Checkbox/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { CheckboxField } from 'payload-plugin-form-builder/dist/types'
+import { CheckboxField } from '@payloadcms/plugin-form-builder/dist/types'
 import { UseFormRegister, FieldErrorsImpl, FieldValues } from 'react-hook-form'
 import { Check } from '../../../_components/icons/Check'
 import { Error } from '../Error'
