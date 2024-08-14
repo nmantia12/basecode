616c1de902de3ca234d495c40571a03321a8fc39
Nick Mantia <nickmantia@Drews-MacBook.lan>
2024-08-14 16:25:42 -0500

    _

--- src/app/(pages)/styleguide/content-block/page.tsx
+++ src/app/(pages)/styleguide/content-block/page.tsx
@@ -29,6 +29,7 @@ export default async function ContentBlockPage() {
                   text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
                 },
               ],
+              blocks:[]
             },
           ]}
         />
